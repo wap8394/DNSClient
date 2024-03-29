7c2907a3ca3064506c05b4ad5e3a3ac2ca43223a
wap8394 <157768094+wap8394@users.noreply.github.com>
2024-03-29 14:28:35 -0500

    Add files via upload

--- DNSClient.py
+++ DNSClient.py
@@ -33,7 +33,7 @@ def query_dns_server(domain, question_type):
 # Define a function to compare the results from the local and public DNS servers for each domain name in the list
 def compare_dns_servers(domainList, question_type):
     for domain_name in domainList:
-        local_ip_address = dns.resolver.Resolve.resolve(domain_name, question_type)
+        local_ip_address = dns.resolver.Resolver.resolve(domain_name, question_type)
         public_ip_address = dns.resolver.Resolver.resolve(domain_name, question_type)
         if local_ip_address != public_ip_address:
             return False
